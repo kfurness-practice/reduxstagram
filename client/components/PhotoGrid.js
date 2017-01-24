be2da369cd58cfbffb5a51f7834357fd8b9c3648
kelly <kelly.n.waite@gmail.com>
2017-01-24 16:18:20 -0700

    Init Repo, Create diff components, Add router

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class PhotoGrid extends React.Component {
+
+  render() {
+    return (
+      <div className="photo-grid">
+          I'm the PhotoGrid
+      </div>
+    )
+  }
+}
---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Single extends React.Component {
+
+  render() {
+    return (
+      <div className="single-photo">
+          I'm the single
+      </div>
+    )
+  }
+}
