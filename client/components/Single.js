07d95827af4b046b4d10debc6297e1183474b9ff
kelly <kelly.n.waite@gmail.com>
2017-02-02 11:17:59 -0700

    Create single component children

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Photo from './Photo';
+import Comments from './Comments';
 
 export default class Single extends React.Component {
-
   render() {
+    // index of post
+    const { postId } = this.props.params
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    // get post
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
     return (
       <div className="single-photo">
-          I'm the single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
       </div>
     )
   }
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,9 +3,21 @@
 // 2. copy of current state
 
 function posts(state = [], action) {
-  console.log('the post will change');
-  console.log(state, action);
-  return state;
+  // take a copy with an object spread for future js
+  // var pic = {...picture};
+  // let pic = Object.assign({}, state);
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      const i = action.index;
+      return [
+        ...state.slice(0,i), //take everything before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), //take everything after the one we are updating
+      ]
+    default:
+      return state;
+  }
+
 }
 
 export default posts;
