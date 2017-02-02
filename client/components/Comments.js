22d8790ecaa4f3a8cbb7f9b8c1e9f4297a50b6d0
kelly <kelly.n.waite@gmail.com>
2017-02-02 14:54:22 -0700

    Remove and add comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,17 +7,26 @@ const Comments = React.createClass({
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.postId, i)}>&times;</button>
         </p>
       </div>
     )
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    console.log(postId, author, comment);
+    this.props.addComment(postId, author, comment);
+    this.ref.commentForm.reset();
   },
   render() {
 
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,34 @@
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT' :
+      // return the new state with the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT' :
+      console.log('Removing a comment 🐈');
+      return [
+        // from the start
+        ...state.slice(0, action.i),
+        // to the one we want to delelte
+        ...state.slice(action.i + 1)
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
-  // console.log(state, action);
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // oerwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
